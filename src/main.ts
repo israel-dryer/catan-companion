56f26cd0ac899a60b048bba79e88c2bafbf6146c
Israel Dryer <israel.dryer@gmail.com>
2024-06-11 09:10:58 -0400

    add pwa

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import {enableProdMode, importProvidersFrom} from '@angular/core';
+import {enableProdMode, importProvidersFrom, isDevMode} from '@angular/core';
 import {bootstrapApplication} from '@angular/platform-browser';
 import {RouteReuseStrategy, provideRouter} from '@angular/router';
 import {IonicRouteStrategy, provideIonicAngular} from '@ionic/angular/standalone';
@@ -7,6 +7,7 @@ import {routes} from './app/app.routes';
 import {AppComponent} from './app/app.component';
 import {environment} from './environments/environment';
 import {provideAnimations} from "@angular/platform-browser/animations";
+import { provideServiceWorker } from '@angular/service-worker';
 
 if (environment.production) {
   enableProdMode();
@@ -14,9 +15,17 @@ if (environment.production) {
 
 bootstrapApplication(AppComponent, {
   providers: [
-    {provide: RouteReuseStrategy, useClass: IonicRouteStrategy},
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
     provideRouter(routes),
     provideAnimations(),
-  ],
+    provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    }),
+    provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })
+],
 });
